5040152c6a689eaeff9577fe83018e3960ab4db0
Justin Le <justin@jle.im>
2017-08-02 01:47:11 -0700

    hotp and totp functions implemented

--- src/Authenticator.hs
+++ src/Authenticator.hs
@@ -3,9 +3,11 @@
 {-# LANGUAGE GADTs               #-}
 {-# LANGUAGE KindSignatures      #-}
 {-# LANGUAGE LambdaCase          #-}
+{-# LANGUAGE RecordWildCards     #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE StandaloneDeriving  #-}
 {-# LANGUAGE TemplateHaskell     #-}
+{-# LANGUAGE TupleSections       #-}
 {-# LANGUAGE TypeApplications    #-}
 {-# LANGUAGE TypeFamilies        #-}
 {-# LANGUAGE TypeInType          #-}
@@ -14,15 +16,18 @@
 module Authenticator (
   ) where
 
+import           Crypto.Hash.Algorithms
 import           Data.Dependent.Sum
 import           Data.Functor.Identity
 import           Data.Kind
-import           Data.OTP
 import           Data.Singletons
 import           Data.Singletons.TH
 import           Data.Text
+import           Data.Time.Clock
+import           Data.Type.Combinator
 import           Data.Type.Conjunction
 import           Data.Type.Equality
+import           Data.Word
 import           GHC.Generics           (Generic)
 import           Type.Class.Higher
 import           Type.Class.Witness
@@ -31,6 +36,7 @@ import qualified Crypto.Gpgme           as GPG
 import qualified Data.Binary            as B
 import qualified Data.ByteString        as BS
 import qualified Data.ByteString.Base32 as B32
+import qualified Data.OTP               as OTP
 import qualified Data.Text              as T
 
 $(singletons [d|
@@ -41,7 +47,7 @@ $(singletons [d|
 instance B.Binary Mode
 
 data family ModeState :: Mode -> Type
-data instance ModeState 'HOTP = HOTPState Integer
+data instance ModeState 'HOTP = HOTPState Word64
   deriving Generic
 data instance ModeState 'TOTP = TOTPState
   deriving Generic
@@ -54,9 +60,21 @@ modeStateBinary = \case
     SHOTP -> Wit1
     STOTP -> Wit1
 
+data HashAlgo = HASHA1 | HASHA256 | HASHA512
+  deriving Generic
+
+instance B.Binary HashAlgo
+
+hashAlgo :: HashAlgo -> SomeC HashAlgorithm I
+hashAlgo HASHA1   = SomeC (I SHA1  )
+hashAlgo HASHA256 = SomeC (I SHA256)
+hashAlgo HASHA512 = SomeC (I SHA512)
+
 data Secret :: Mode -> Type where
     Sec :: { secAccount :: T.Text
            , secIssuer  :: Maybe T.Text
+           , secAlgo    :: HashAlgo
+           , secDigits  :: Word
            , secKey     :: BS.ByteString
            }
         -> Secret m
@@ -78,8 +96,23 @@ instance B.Binary (DSum Sing (Secret :&: ModeState)) where
         B.put ms
 
 data Store = Store { storeList :: [DSum Sing (Secret :&: ModeState)] }
+-- data Store = Store { storeHOTP :: [(Secret 'HOTP, Integer)]
+--                    , storeTOTP :: [Secret 'TOTP]
+--                    }
   deriving Generic
 
 instance B.Binary Store
 
+hotp :: Secret 'HOTP -> ModeState 'HOTP -> (Word32, ModeState 'HOTP)
+hotp Sec{..} (HOTPState i) = (p, HOTPState (i + 1))
+  where
+    p = hashAlgo secAlgo >>~ \(I a) -> OTP.hotp a secKey i secDigits
 
+totp :: Secret 'TOTP -> UTCTime -> Word32
+totp Sec{..} t = hashAlgo secAlgo >>~ \(I a) ->
+    OTP.totp a secKey t 30 secDigits
+
+otp :: forall m. SingI m => Secret m -> ModeState m -> IO (Word32, ModeState m)
+otp = case sing @_ @m of
+    SHOTP -> \s ms -> return $ hotp s ms
+    STOTP -> \s ms -> ((, ms) . totp s) <$> getCurrentTime
